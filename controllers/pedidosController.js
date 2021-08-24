9b19a67740844a2099694b4a8849ea016a47b25c
henriquepbalsimelli <henriquepbalsimelli@outlook.com>
2021-08-24 09:43:13 -0300

    adiciona funcionalidade de buscar último pedido e somar um

--- controllers/pedidosController.js
+++ controllers/pedidosController.js
@@ -2,32 +2,35 @@ const models = require('../models');
 
 module.exports.renderizaForm = (async(req,res,next) =>{
     const produtos = await models.Produtos.findAll()
+    const ultimoPedido = await models.Pedidos.findOne({
+        where:{},
+        order:[['id', 'DESC']]
+    })
     
     res.render('formCadastro', {
-        produtos: produtos
+        produtos: produtos,
+        pedidos: ultimoPedido
     })
 })
 
 module.exports.enviaPedido = (async(req,res,next)=>{
     const produtos = await models.Produtos.findAll()
-    const informacoes = req.body
 
+    const informacoes = req.body
+    
     await models.Pedidos.create({
         nomeCliente: req.body.nomeCliente,
         dataDeEntrega: req.body.dataDeEntrega
     })
 
-    let pedidoId = await models.Pedidos.findAll({
-        where: {
-            id: req.params.id
-        }
-    })
+    
     let produtoId = req.body.produtoId
     let quantidade = req.body.quantidade
-    
+    let pedidoId = parseInt(req.body.pedidoId)
+
     for (var i =0; i< quantidade.length; i++){
         models.Pedidos_produtos.create({
-            pedidoId: req.params.id,
+            pedidoId: pedidoId,
             produtoId: produtoId[i],
             quantidade: quantidade[i]
         })
